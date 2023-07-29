9df9f6fc5ea195a1d2a933d64460bceb8c29274d
Uthmanbello <uthmanbello88@gmail.com>
2023-07-29 23:05:09 +0100

    Setup landing page

--- src/courses/courses/settings.py
+++ src/courses/courses/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my own apps
+    'courselist.apps.CourselistConfig',
 ]
 
 MIDDLEWARE = [
--- src/courses/courses/urls.py
+++ src/courses/courses/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from courselist.views import landing_page
 
 urlpatterns = [
+    path('', landing_page, name='landing_page'),
     path('admin/', admin.site.urls),
 ]
