accf1227ea78fd0e0b9499f91e02e3a60c6f8623
Uthmanbello <uthmanbello88@gmail.com>
2023-07-31 20:06:09 +0100

    Style the add course page

--- src/courses/courselist/forms.py
+++ src/courses/courselist/forms.py
@@ -5,3 +5,7 @@ class CourseForm(forms.ModelForm):
     class Meta:
         model = Courselist
         fields = ['name', 'icon']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'text-field secondary'}),
+            'icon': forms.Select(attrs={'class': 'select-field secondary'}),
+        }
