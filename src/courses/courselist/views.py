ad2edcaaed305b65fb173c971b74bc0a613a71ee
Uthmanbello <uthmanbello88@gmail.com>
2023-08-02 23:57:35 +0100

    Add delete topic functionality

--- src/courses/courselist/views.py
+++ src/courses/courselist/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Count, Sum
-from .models import Courselist
+from .models import Courselist, Topic
 from .forms import CourseForm, TopicForm
 
 # Create your views here.
@@ -50,3 +50,11 @@ def add_topics(request, courselist_id):
         form = TopicForm()
 
     return render(request, 'add_topics.html', {'form': form})
+
+def delete_topic(request, courselist_id, topic_id):
+    course = get_object_or_404(Courselist, id=courselist_id)
+    topic = get_object_or_404(Topic, id=topic_id)
+    if request.method == 'POST':
+        topic.delete()
+        return redirect('course_detail', courselist_id=course.id)
+    return render(request, 'delete_topic.html', {'course': course, 'topic': topic})
