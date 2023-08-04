5ba2c9a9b2f10093bb7e3dbb2647d19502ecf086
Uthmanbello <uthmanbello88@gmail.com>
2023-08-04 10:13:34 +0100

    Adjust models to accomodate icons

--- src/courses/courselist/models.py
+++ src/courses/courselist/models.py
@@ -4,11 +4,21 @@ from django.db import models
 class Courselist(models.Model):
     name = models.CharField(max_length=100)
     ICON_CHOICES = [
-        ('icon1', 'Icon 1'),
-        ('icon2', 'Icon 2'),
-        ('icon3', 'Icon 3'),
+        ('python', 'python'),
+        ('javascript', 'javascript'),
+        ('c', 'c'),
+        ('css', 'css'),
+        ('java', 'java'),
+        ('swift', 'swift'),
+        ('scala', 'scala'),
+        ('kotlin', 'kotlin'),
+        ('php', 'php'),
+        ('ruby', 'ruby'),
+        ('rust', 'rust'),
+        ('html', 'html'),
+        ('default', 'default'),
     ]
-    icon = models.CharField(max_length=20, choices=ICON_CHOICES, default='default_icon')
+    icon = models.CharField(max_length=20, choices=ICON_CHOICES, default='default')
 
 class Topic(models.Model):
     course = models.ForeignKey(Courselist, on_delete=models.CASCADE, related_name='topics')
