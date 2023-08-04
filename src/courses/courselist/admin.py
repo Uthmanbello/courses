f5a46b14692c56cdff78e2f429bacb88f19edb1e
Uthmanbello <uthmanbello88@gmail.com>
2023-08-04 06:00:42 +0100

    Setup some links

--- src/courses/courselist/admin.py
+++ src/courses/courselist/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Courselist, Topic
-admin.site.register(Courselist, Topic)
+admin.site.register(Courselist)
+admin.site.register(Topic)
